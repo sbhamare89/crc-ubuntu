b67b35dde596d3284c6bfc9d3e59aee40ef41464
Sagar  Bhamare <80466696+sbhamare89@users.noreply.github.com>
2021-11-10 14:31:18 +0530

    Update 2-create-crc.sh

--- 2-create-crc.sh
+++ 2-create-crc.sh
@@ -40,7 +40,11 @@ sudo systemctl restart dnsmasq
 dig foo.apps-crc.testing | echo $(grep 192.168.130.11)
 dig api.crc.testing | echo $(grep 192.168.130.11)
 
-curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/crc/1.9.0/crc-linux-amd64.tar.xz && tar -Jxvf crc-linux-amd64.tar.xz && mkdir -p ~/bin && export PATH=$PATH:~/bin && mv crc-linux*/crc ~/bin/ && rm -rf crc*
+
+4.5.1
+
+curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/crc/1.13.0/crc-linux-amd64.tar.xz && tar -Jxvf crc-linux-amd64.tar.xz && mkdir -p ~/bin && export PATH=$PATH:~/bin && mv crc-linux*/crc ~/bin/ && rm -rf crc*
+#curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/crc/1.9.0/crc-linux-amd64.tar.xz && tar -Jxvf crc-linux-amd64.tar.xz && mkdir -p ~/bin && export PATH=$PATH:~/bin && mv crc-linux*/crc ~/bin/ && rm -rf crc*
 #curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/crc/latest/crc-linux-amd64.tar.xz && tar -Jxvf crc-linux-amd64.tar.xz && mkdir -p ~/bin && export PATH=$PATH:~/bin && mv crc-linux*/crc ~/bin/ && rm -rf crc*
 
 crc config set skip-check-network-manager-installed true
